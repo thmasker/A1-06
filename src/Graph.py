a50e6a133dd2b41b20d8aeca7a2af539a7af2e49
thmasker <diego.pedregal@alu.uclm.es>
2018-10-02 12:03:41 +0200

    creation

---
+++ src/Graph.py
@@ -0,0 +1,14 @@
+import xml.etree.ElementTree as ET
+
+class Graph:
+    def __init__(self, file):
+        self.file = file
+        self.graph = ET.parse('self.file').getroot()
+
+    def BelongNode(id):
+
+
+    def positionNode(id):
+
+
+    def adjacentNode(id):

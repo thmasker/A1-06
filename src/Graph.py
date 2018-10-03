955e8409b0bd06cd31d25e492193c06e91149d7b
Pablo <palcamora@gmail.com>
2018-10-03 11:18:37 +0200

    Belong Node implemetation DONE

--- src/Graph.py
+++ src/Graph.py
@@ -1,14 +1,32 @@
 import xml.etree.ElementTree as ET
 
 class Graph:
-    def __init__(self, file):
-        self.file = file
-        self.graph = ET.parse('self.file').getroot()
+    def __init__(self, graphml):
+        self.file = graphml
+        self.graph = ET.parse(self.file).getroot()
 
-    def BelongNode(id):
+        self.ns = { 'default': 'http://graphml.graphdrawing.org/xmlns' } # ns = name space
+
+        self.nodes = self.graph.findall('default:graph/default:node', self.ns)
 
 
-    def positionNode(id):
+    def BelongNode(self, nodeid):
+        isNode = False
+
+        for node in self.nodes:
+            if nodeid == node.get('id'):
+                isNode = True
+                break
+
+        return isNode
 
 
-    def adjacentNode(id):
+
+    #def positionNode(nodeid):
+
+
+    #def adjacentNode(nodeid):
+
+if __name__ == '__main__':
+    anchuras = Graph('data/Anchuras.graphml')
+    anchuras.BelongNode('4928063639')

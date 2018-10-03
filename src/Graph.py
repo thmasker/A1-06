df8eceb67afac7c6669bf7128a4dfa2e71612ec5
thmasker <diego.pedregal@alu.uclm.es>
2018-10-03 18:26:51 +0200

    adjacentNode doing

--- src/Graph.py
+++ src/Graph.py
@@ -5,10 +5,9 @@ class Graph:
         self.file = graphml
         self.graph = ET.parse(self.file).getroot()
 
-        self.ns = { 'default': 'http://graphml.graphdrawing.org/xmlns' } # ns = name space
-
+        self.ns = {'default': 'http://graphml.graphdrawing.org/xmlns'}  # ns = name space
         self.nodes = self.graph.findall('default:graph/default:node', self.ns)
-
+        self.edges = self.graph.findall('default:graph/default:edge', self.ns)
 
     def belongNode(self, nodeid):
         for node in self.nodes:
@@ -24,9 +23,19 @@ class Graph:
 
         return '[ERROR] Node does not exist on the graph'
 
+    def adjacentNode(self, nodeid):
+        adjacencyList = []
 
-    #def adjacentNode(nodeid):
+        if self.belongNode(nodeid):
+            for edge in self.edges:
+                if edge.get('source') == nodeid:
+                    adjacencyList.append((edge.get('source'), edge.get('target'),
+                                          edge[1].text if edge[1].get('key') == 'd13' else 'SinNombre', edge[4].text))
+        else:
+            adjacencyList = '[ERROR] Node does not exist on the node'
+
+        return adjacencyList
 
 if __name__ == '__main__':
     anchuras = Graph('../data/Anchuras.graphml')
-
+    print(anchuras.adjacentNode('4331489627'))

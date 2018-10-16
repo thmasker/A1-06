44892e828416df2919ea129d4481f4b7e3b2ad3a
thmasker <diego.pedregal@alu.uclm.es>
2018-10-16 18:35:55 +0200

    Frontier.py created

---
+++ src/Frontier.py
@@ -0,0 +1,12 @@
+
+"""
+Class Name: Frontier
+Class description:
+"""
+class Frontier:
+    """
+    Method name:    __init__
+    Description of the Method:  Constructor
+    Calling arguments:
+    """
+    def __init__(self):

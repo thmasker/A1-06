c6be441b06b76ef99043a94243c501d9f2508d9b
thmasker <diego.pedregal@alu.uclm.es>
2018-10-16 19:22:55 +0200

    Problem finished

--- src/Problem.py
+++ src/Problem.py
@@ -1,21 +1,36 @@
 import json
-import State
-import StateSpace
+import src.State as S
+import src.StateSpace as SP
 
+"""
+Class Name: Problem
+Description:    Class defining the problem to solve
+"""
 class Problem:
-
+    """
+    Method name:    __init__
+    Description:    Constructor
+    Calling arguments:  jsonPath: path to the json file to read
+    Checked Exceptions: FileNotFoundError: checks if the .json file exists or not
+    """
     def __init__(self, jsonPath):
-        with open(jsonPath, "r") as rstate:
-            rdata = json.load(rstate)
-        
-        self.StateSpace = StateSpace(rdata["graphmlfile"])
-        self.InitState = State(rdata["IntSt"]["node"], rdata["IntSt"]["listNodes"])
+        try:
+            with open(jsonPath, "r") as rstate:
+                rdata = json.load(rstate)
+        except FileNotFoundError as fnf_error:
+            print(fnf_error)
+            return
 
+        self.StateSpace = SP.StateSpace(rdata["graphmlfile"])
+        self.InitState = S.State(rdata["IntSt"]["node"], rdata["IntSt"]["listNodes"])
+
+    """
+    Method name:    isGoal
+    Description:    checks if the problem is already solved
+    Calling arguments:  state: current state
+    Return value:   True if problem is solved; False otherwise
+    """
     def isGoal(self, state):
         if not state.nodesRemaining:
             return True
         return False
-
-
-
-Problem("input.json")

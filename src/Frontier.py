63455f2e544d8bba16bf328d3f7c6e0f88790861
thmasker <diego.pedregal@alu.uclm.es>
2018-10-16 19:24:24 +0200

    Frontier.py created

--- src/Frontier.py
+++ src/Frontier.py
@@ -1,12 +1,37 @@
-
 """
 Class Name: Frontier
-Class description:
+Description:  Implementation of a tree frontier list
 """
 class Frontier:
     """
-    Method name:    __init__
-    Description of the Method:  Constructor
-    Calling arguments:
+    Method name:    createFrontier
+    Description:  It creates an empty frontier
     """
-    def __init__(self):
+    def createFrontier(self):
+
+
+    """
+    Method name:    insert
+    Description:    adds a new TreeNode to the frontier
+    Calling arguments:  treeNode: TreeNode to insert in the frontier
+    Required Files:
+    Checked Exceptions:
+    """
+    def insert(self, treeNode):
+
+
+    """
+    Method name:    remove
+    Description:    takes the first element in the frontier and removes from it
+    Return value:
+    """
+    def remove(self):
+
+
+    """
+    Method name:    isEmpty
+    Description:    checks if the frontier is empty or not
+    Return value:   True if frontier is empty; False otherwise
+    Checked Exceptions:
+    """
+    def isEmpty(self):

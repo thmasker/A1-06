210f71c983a3462391f45e26382faad496dd1661
thmasker <diego.pedregal@alu.uclm.es>
2018-10-25 13:54:40 +0200

    Frontier updated

--- src/Frontier.py
+++ src/Frontier.py
@@ -11,44 +11,38 @@ class Frontier:
     Description:  Constructor. It creates an empty frontier
     """
     def __init__(self):
-        self.queue = blist([])
+        self.frontier = blist([])
 
     """
     Method name:    insert
     Description:    adds a new TreeNode to the frontier
     Calling arguments:  treeNode: TreeNode to insert in the frontier
-    Required Files:
-    Checked Exceptions:
+    Checked Exceptions: MemoryError: if used memory is more than a 75 %, program must stop
+                            its execution
     """
     def insert(self, treeNode):
         if psutil.swap_memory()[3] >= 75:
             print("Not enough space in memory")
             raise MemoryError
         else:
-            self.queue.append(treeNode)
+            self.frontier.insert(treeNode.f, treeNode)
 
     """
     Method name:    remove
     Description:    takes the first element in the frontier and removes from it
-    Return value:
+    Return value:   Element removed from the frontier
     """
     def remove(self):
         try:
-            low = 0
-            for i in range(len(self.queue)):
-                if self.queue[i].f < self.queue[low].f:
-                    low = i
-            item = self.queue.pop(i)
-            return item
+            return self.frontier.pop(0)
         except IndexError:
             print()
-            exit()
+            raise SystemExit
 
     """
     Method name:    isEmpty
     Description:    checks if the frontier is empty or not
     Return value:   True if frontier is empty; False otherwise
-    Checked Exceptions:
     """
     def isEmpty(self):
-        return len(self.queue) == blist([])
+        return len(self.frontier) == blist([])

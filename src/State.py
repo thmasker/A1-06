05e9dbe373948e4ba1f0fa674f620a7cf5509726
thmasker <diego.pedregal@alu.uclm.es>
2018-11-09 11:53:06 +0100

    State fixed

--- src/State.py
+++ src/State.py
@@ -14,63 +14,6 @@ class State:
     def __init__(self, currentPosition, nodesRemaining):
         self.currentPosition = currentPosition
         self.nodesRemaining = nodesRemaining
-        try:
-            self.mergesortNodes(self.nodesRemaining, 0, len(self.nodesRemaining) - 1)
-        except TypeError:
-            pass
-
+        self.nodesRemaining.sort()
         self.md5checksum = hashlib.md5((str(self.currentPosition) + ",".join(str(self.nodesRemaining))).encode())
 
-    """
-    Method name:    mergesortNodes
-    Description:  sorts the list passed as input following ascendent numerical order
-    Calling arguments:  - nodesList: list of node ids to order
-                        - leftIndex: nodesList starting position of the sublist to order
-                        - rightIndex: nodesList last position of the sublist to order
-    """
-    def mergesortNodes(self, nodesList, leftIndex, rightIndex):
-        if leftIndex < rightIndex:
-            middleIndex = (leftIndex + rightIndex) // 2
-            self.mergesortNodes(nodesList, leftIndex, middleIndex)
-            self.mergesortNodes(nodesList, middleIndex+1, rightIndex)
-            self.mergeNodes(nodesList, leftIndex, middleIndex, rightIndex)
-
-    """
-    Method name:    mergeNodes
-    Description:  sorts the list passed as input following ascendent numerical order
-    Calling arguments:  - nodesList: list of node ids to order
-                        - leftIndex: nodesList starting position of the sublist to order
-                        - middleIndex: nodesList middle position of the sublist to order
-                        - rightIndex: nodesList last position of the sublist to order
-    """
-    def mergeNodes(self, nodesList, leftIndex, middleIndex, rightIndex):
-        k = leftIndex
-        tempLeft, tempRight = [0] * (middleIndex - leftIndex + 1), [0] * (rightIndex - middleIndex)
-
-        for i in range(0, middleIndex - leftIndex + 1):
-            tempLeft[i] = nodesList[leftIndex + i]
-
-        for j in range(0, rightIndex - middleIndex):
-            tempRight[j] = nodesList[middleIndex + 1 + j]
-
-        i, j = 0, 0
-
-        while (i < len(tempLeft)) and (j < len(tempRight)):
-            if tempLeft[i] <= tempRight[j]:
-                nodesList[k] = tempLeft[i]
-                i += 1
-            else:
-                nodesList[k] = tempRight[j]
-                j += 1
-
-            k += 1
-
-        while i < len(tempLeft):
-            nodesList[k] = tempLeft[i]
-            i += 1
-            k += 1
-
-        while j < len(tempRight):
-            nodesList[k] = tempRight[j]
-            j += 1
-            k += 1
